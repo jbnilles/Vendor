b219a4261beff1e6e04a9e844fcc773ba27177e1
jbnilles <jbnilles24@gmail.com>
2020-10-02 10:10:14 -0700

    add tests for orders

--- VendorTracker.Tests/ModelTests/OrderTests.cs
+++ VendorTracker.Tests/ModelTests/OrderTests.cs
@@ -1,7 +1,25 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorTracker.Models;
+
 namespace VendorTracker.Tests
 {
-    public class OrderTests
+  [TestClass]
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-        
+      Order newOrder = new Order("title","desc",2.2,DateTime.Now, 1);
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    
+    
+   
+   
+
+
+  }
 }
+
